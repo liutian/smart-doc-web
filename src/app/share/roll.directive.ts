5224541810f8ab17dc9e1e8bd7bd0d24795e69c6
liuss <liuss@visionet.com.cn>
2017-09-01 12:03:09 +0800

    调整文章滚动效果

--- src/app/share/roll.directive.ts
+++ src/app/share/roll.directive.ts
@@ -14,27 +14,42 @@ import {
 export class RollDirective {
   stepSection = [];
   currStepIndex;
+  private lastScrollTop;
 
   @Input() stepSelector: string;
   @Output() rollTrigger = new EventEmitter<number>();
   @Input() set currStep(index) {
-    window.document.body.scrollTop = this.stepSection[index];
+    window.document.body.scrollTop = this.stepSection[index] - this.offset;
   }
+  @Input() offset = 0;
 
   constructor(private ele: ElementRef) { }
 
-  @HostListener('document:scroll', ['$event']) onscroll(e) {
-    const scroll = window.document.body.scrollTop;
+  @HostListener('document:mousewheel', ['$event']) onscroll(e) {
+    let scroll = window.document.body.scrollTop;
+    const diff = scroll - this.lastScrollTop;
+    this.lastScrollTop = scroll;
 
-    for (let i = 0; i < this.stepSection.length; i++) {
-      if (scroll < this.stepSection[i]) {
-        if (this.currStepIndex === i) {
-          return;
+    if (diff === 0) {
+      if (e.wheelDelta < 0 && this.currStepIndex < this.stepSection.length - 1) {
+        ++this.currStepIndex;
+        this.rollTrigger.emit(this.currStepIndex);
+      } else if (e.wheelDelta > 0 && this.currStepIndex > 0) {
+        --this.currStepIndex;
+        this.rollTrigger.emit(this.currStepIndex);
+      }
+    } else {
+      scroll += this.offset;
+      for (let i = this.stepSection.length - 1; i > 0; i--) {
+        if (scroll > this.stepSection[i] - 20) {
+          if (this.currStepIndex === i) {
+            break;
+          }
+
+          this.rollTrigger.emit(i);
+          this.currStepIndex = i;
+          break;
         }
-
-        this.rollTrigger.emit(i);
-        this.currStepIndex = i;
-        return;
       }
     }
   }
