cbd9a59d2049ccda513239365722dc0c244a9c7f
liuss <liuss@visionet.com.cn>
2017-09-20 18:20:21 +0800

    修复bug

--- src/app/admin/manual-save/manual-save.component.ts
+++ src/app/admin/manual-save/manual-save.component.ts
@@ -45,7 +45,7 @@ export class ManualSaveComponent implements OnInit {
   }
 
   onUpload(e) {
-    this.formData.cover = e[2].files.fileList[0].path;
+    this.formData.cover = e[2].fileList[0].path;
   }
 
   submit() {
