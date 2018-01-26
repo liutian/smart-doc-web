2f651f88042efd7f9b95b5168ce1fec4e8e7b9fe
liuss <liuss@visionet.com.cn>
2018-01-26 16:47:48 +0800

    更新登陆页

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  styles: [`
+    :host{
+      display: block;
+    }
+  `],
   template: '<router-outlet></router-outlet>'
 })
 export class AppComponent {
